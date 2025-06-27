b0786921158e66ef8c6bf51f4a216c6ded06b4ca
Aarisha <Goyal.Aarisha@gmail.com>
2025-06-26 18:53:34 -0700

    eighth change

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,17 +2,26 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "index";
+        return "Home";
     }
 
-    @GetMapping("/HI")
-    public String HI() {
-        return "Welcome, Aarisha!";
+  
+    @PostMapping("/greet")
+    public String greet(String username, Model model) {
+        model.addAttribute("userloggedin", username);
+        System.out.println("username: " + username );
+        return "result";
     }
 }
+
+
+    
+  
